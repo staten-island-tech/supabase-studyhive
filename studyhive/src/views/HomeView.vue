5edf93debb90312233d34c2abdc98f1931ef2cda
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-04-07 12:10:47 -0400

    data collection example component

--- studyhive/src/views/HomeView.vue
+++ studyhive/src/views/HomeView.vue
@@ -1,31 +1,13 @@
+<template>
+  <div>
+    <DataCollection />
+  </div>
+</template>
+
 <script setup>
-import { ref, onMounted } from 'vue';
-import { supabase } from '@/supabase.js';
-
-const messages = ref([]);
-
-async function getMessages() {
-  let { data: users, error } = await supabase
-  .from('users')
-  .select('*')
-
-  if (error) {
-    console.error('Error fetching messages:', error);
-    return;
-  }
-
-  console.log(users);
-}
-
-onMounted(() => {
-  getMessages();
-});
+import DataCollection from '@/components/DataCollection.vue';
 </script>
 
-<template>
-  <ul>
-    <li v-for="message in messages" :key="message.id">
-      {{ message.content }}
-    </li>
-  </ul>
-</template>
+<style scoped>
+
+</style>
