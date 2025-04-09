493d933fe66c4c600b75e9a1a9b0cd6913f242d1
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-04-09 12:32:34 -0400

    safdsafdsafas

--- studyhive/src/components/ChangeProfile.vue
+++ studyhive/src/components/ChangeProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <DataCollection />
+
   </div>
 </template>
 
---
+++ studyhive/src/views/GetStarted.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class=" bg-amber-700 w-full h-screen">
+        <button class="">Get Started >></button>
+        <div class="flex justify-center">
+            <nav class="w-3/4 h-10 bottom-1/9 bg-amber-400 fixed">
+                <ul class="flex justify-around items-center h-full">
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+   
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
