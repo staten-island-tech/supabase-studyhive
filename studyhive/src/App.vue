143552b4f7f903977d7e25fc132f6dbf91cf609b
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-04-10 12:31:20 -0400

    dasdas

--- studyhive/src/App.vue
+++ studyhive/src/App.vue
@@ -5,12 +5,11 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+   <!--  <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
-      <nav>
+      <nav class="absolute ">
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/getStarted ">getstarted</RouterLink>
       </nav>
--- studyhive/src/views/GetStarted.vue
+++ studyhive/src/views/GetStarted.vue
@@ -1,16 +1,20 @@
 <template>
-    <div class=" bg-amber-700 w-full h-screen">
-        <button class="">Get Started >></button>
-        <div class="flex justify-center">
-            <nav class="w-3/4 h-10 bottom-1/9 bg-amber-400 fixed">
-                <ul class="flex justify-around items-center h-full">
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Contact</a></li>
-                </ul>
-            </nav>
+    <div class="w-screen h-screen flex justify-center items-center flex-col ">
+        <nav class="w-full h-18 rounded-[10rem] top-1/9 ">
+                    <ul class="flex justify-around items-center h-full">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+        <div class=" bg-amber-700 w-[96%] h-[96%] rounded-[6rem]">
+            <button class="w-[10rem] h-[3rem] bg-green-500 absolute ml-20 mt-50">Get Started >></button>
+            <div class="flex justify-center">
+                
+            </div>
         </div>
     </div>
+
    
 </template>
 
