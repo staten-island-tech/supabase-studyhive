bb5354f5a62b1470654e8ba23ebe1cc1cc5c260c
elisa-23 <158204265+elisa-23@users.noreply.github.com>
2025-04-19 18:40:43 -0400

    supabase update

--- studyhive/src/supabase.ts
+++ studyhive/src/supabase.ts
@@ -1,13 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://baoafwwjrcrstirukkkb.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhb2Fmd3dqcmNyc3RpcnVra2tiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2OTU5NjcsImV4cCI6MjA1OTI3MTk2N30.W1JG44azGV5d7R6wd3GCGEr8UEQZe9SoBRfcu4klWEA';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-const { data, error } = await supabase.auth.signUp({
-    email: 'elisa@example.com',
-    password: 'supasecret123',
-});
-
-
