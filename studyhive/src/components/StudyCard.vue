88a30de77ef25c85915192013827f976ea4443cd
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-05-30 12:56:20 -0400

    thunk

--- studyhive/src/components/StudyCard.vue
+++ studyhive/src/components/StudyCard.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="card card-border bg-base-100 w-[30%]">
-    <div class="p-5 flex flex-col gap-3">
-      <h2 class="card-title"></h2>
-      <div class="flex gap-2">
-        <p class="terms"></p>
-        <img src="/img/image.png" alt="" class="w-[1.5rem] rounded-[4px]" />
+  <div class="w-[30%] p-[2px] rounded-3xl bg-gradient-to-r from-amber-500 to-pink-400">
+    <div class="bg-base-100 rounded-3xl">
+      <div class="p-5 flex flex-col gap-3">
+        <h2 class="card-title font-bold">super cool study set</h2>
+        <div class="flex mb-10">
+          <p class="terms bg-amber-300 px-3 py-1 rounded-2xl font-semibold text-[0.9rem]">
+            12 terms
+          </p>
+        </div>
+        <div class="user flex gap-2 items-center">
+          <img src="/img/image.png" alt="" class="w-[7%] rounded-3xl" />
+          <p class="user">josephine_liu43</p>
+        </div>
       </div>
-      <div class="user flex gap-2 items-center">
-        <img src="/img/image.png" alt="" class="w-[7%] rounded-3xl" />
-        <p></p>
-      </div>
-
-      <button class="btn bg-gradient-to-r from-amber-500 to-pink-400 text-white rounded-3xl">
-        Check Set
-      </button>
     </div>
   </div>
 </template>
--- studyhive/src/views/SetsView.vue
+++ studyhive/src/views/SetsView.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="mt-30 bg-green-400 py-10 flex flex-row justify-around">
-    <!-- <StudyCard :test="test" /> -->
+  <div class="pt-[10rem] bg-[#F6F7FB] py-10 flex flex-row justify-around">
+    <StudyCard />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import StudyCard from '@/components/StudyCard.vue'
+</script>
 
 <style scoped></style>
