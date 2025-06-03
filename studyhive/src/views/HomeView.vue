6ffdc37097e6855be6cbd1402a34db724a3bbd07
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-06-03 12:31:23 -0400

    safasf

--- studyhive/src/views/HomeView.vue
+++ studyhive/src/views/HomeView.vue
@@ -1,7 +1,13 @@
 <template>
-  <div class="w-full h-[90rem] bg-green-800 gap-9 items-start pt-48 flex flex-row">
-    <div class="">
+  <div class="w-full h-[90rem] bg-green-800 gap-9 justify-around items-start pt-48 flex flex-row">
+    <div class="w-[60%] aspect-[4/1] bg-amber-500 rounded-2xl flex">
       <h1>WELCOME BACK JOE</h1>
+      <p>
+        dsiadhua udhsuik hu fukafhds fd hu hui dhui dfsaiuh fsdauhi sdfihudf hu ifdauih df uih fduhi
+        dfsihusiuh dfsauhi sdfa uih as
+      </p>
+
+      <img src="/img/image.png" alt="" class="w-10 aspect-square" />
     </div>
   </div>
 </template>
