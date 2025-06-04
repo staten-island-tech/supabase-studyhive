753431f7888a3a5ec91b77d54ad2467fe0f51313
jo-svg <144480253+jo-svg@users.noreply.github.com>
2025-06-03 22:31:54 -0400

    fdsfds

--- studyhive/src/components/StudyCard.vue
+++ studyhive/src/components/StudyCard.vue
@@ -2,21 +2,36 @@
   <div class="w-[30%] p-[2px] rounded-3xl bg-gradient-to-r from-amber-500 to-pink-400">
     <div class="bg-base-100 rounded-3xl">
       <div class="p-5 flex flex-col gap-3">
-        <h2 class="card-title font-bold">super cool study set</h2>
+        <h2 class="card-title font-bold">{{title}}</h2>
         <div class="flex mb-10">
           <p class="terms bg-amber-300 px-3 py-1 rounded-2xl font-semibold text-[0.9rem]">
-            12 terms
+            {{num}} terms
           </p>
         </div>
         <div class="user flex gap-2 items-center">
           <img src="/img/image.png" alt="" class="w-[7%] rounded-3xl" />
-          <p class="user">josephine_liu43</p>
+          <p class="user">{{user}}</p>
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  num: {
+    type: Number,
+    required: true
+  },
+  user: {
+    type: String,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+});
+</script>
 
 <style scoped></style>
--- studyhive/src/views/SetsView.vue
+++ studyhive/src/views/SetsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="pt-[10rem] bg-[#F6F7FB] py-10 flex flex-row justify-around">
-    <StudyCard />
+    <StudyCard :num=12 title="very cool thing yk" user="josephine_liu43"/>
   </div>
 </template>
 
